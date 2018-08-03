9568cf63a350c812f037d04285bce53636529790
unknown <ushinokomoriuta@gmail.com>
2018-08-03 09:57:31 +0900

    setting.pyにgunicornを追記

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #herokuでgunicornの説明があるので追記
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
